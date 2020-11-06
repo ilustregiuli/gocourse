6707809d755c1ed470a9d84d2cde5427544e300d
Giuli Ilustre <ilustre.giuli@gmail.com>
2020-11-05 21:39:40 -0300

    Exercicio 1, parte 2

---
+++ nivel_02_Exer_01.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+// fazer um programa que mostre um número no formato decimal, binário e hexadecimal
+
+func main() {
+
+	x := 200
+	fmt.Printf("%d = decimal\n%b = binário\n%#x = hexadecimal", x,x,x)
+	
+}
