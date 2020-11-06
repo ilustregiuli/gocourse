37384a133a6beba8491a0aa0ab643a57ffc80daf
Giuli Ilustre <ilustre.giuli@gmail.com>
2020-11-05 21:59:24 -0300

    Exercicio 3, parte 2

---
+++ nivel_02_Exer_03.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+
+//	Crie constantes tipadas e não tipadas
+	
+	const giuli = 10
+	const ilustre bool = true
+
+func main() {
+	
+	fmt.Printf("%T\n", giuli)
+	fmt.Printf("%T\n", ilustre)
+	
+}
