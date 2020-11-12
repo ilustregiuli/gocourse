b0a933681f76aa8d8cdf7f088dac9ec7179764b6
Giuli Ilustre <ilustre.giuli@gmail.com>
2020-11-11 21:10:04 -0300

    exercicio 5 parte 2

---
+++ nivel_02_Exer_05.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+//Crie uma variável de tipo string utilizando uma raw string literal
+	
+
+func main() {
+	
+	meuGalo := ` 
+	
+		     Giuli Ilustre
+			  is
+		     The Best Programmer 
+						`
+		
+	fmt.Println(meuGalo)		
+		
+	
+	
+}
